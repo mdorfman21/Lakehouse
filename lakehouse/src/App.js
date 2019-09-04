65cf00954c6b5e7fe6e9253c4bed027e1f98c844
unknown <mdorfman21@gmail.com>
2019-09-04 15:56:46 -0400

    working sliders

--- lakehouse/src/App.js
+++ lakehouse/src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Images from './components/ImageGallery';
 
 function App() {
   return (
+    <div>
+
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h2>Lakehouse</h2>
+     
+    </div>
+    <div>
+      <Images />
+    </div>
     </div>
   );
 }
